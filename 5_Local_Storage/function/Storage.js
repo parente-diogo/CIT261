173a024e304539833919106a1f02d699a0f90ed2
Diogo Parente <31190447+parente-diogo@users.noreply.github.com>
2020-01-22 23:40:24 -0300

    a

--- 5_Local_Storage/function/Storage.js
+++ 5_Local_Storage/function/Storage.js
@@ -1,6 +1,6 @@
 // VERIFY SUPPORT FOR STORAGE FUNCTIONS --------------------------------------------- //
-function support() {  //Check whether the browser support Storage Functions
-  if (typeof(Storage) !== "undefined") {
+function support() { //Check whether the browser support Storage Functions
+  if (typeof (Storage) !== "undefined") {
     alert("Your browser support Storage Functions.");
   } else {
     alert("Your browser does NOT support Storage Functions.\nPlease update your browser.");
@@ -26,7 +26,6 @@ function clearLocal() {
   localStorage.clear();
 }
 
-
 // STORE SOME DATA FOR THIS SESSION ------------------------------------------------- //
 function storeSession() {
   sessionStorage.setItem("testSession", "This will expired when closing the tab.");
@@ -44,4 +43,30 @@ function eraseSession() {
 
 function clearSession() {
   sessionStorage.clear();
+}
+
+// WORKING WITH ARRAYS -------------------------------------------------------------- //
+function storeArray() {
+  var fruits = ["Apple", "Orange", "Strawberry", "Grapes", "Pineapple"];
+  localStorage.setItem("my fruits", fruits);
+}
+
+function storeArray2() {
+  var fruits = ["Apple", "Orange", "Strawberry", "Grapes", "Pineapple"];
+  localStorage.setItem("same fruits", JSON.stringify(fruits));
+}
+
+function getArray() {
+  var fruits = localStorage.getItem("my fruits");
+  alert(fruits);
+  alert(typeof (fruits));
+  alert(fruits.length);
+}
+
+function getArray2() {
+  var fruits = localStorage.getItem("same fruits");
+  var fruits2 = JSON.parse(fruits);
+  alert(fruits2);
+  alert(typeof (fruits2));
+  alert(fruits2.length);
 }
