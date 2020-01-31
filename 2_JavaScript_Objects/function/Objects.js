a03d141086166e0204d2f7b46aac2520d6a0c90f
Diogo Parente <31190447+parente-diogo@users.noreply.github.com>
2020-01-30 21:33:03 -0300

    a

--- 2_JavaScript_Objects/function/Objects.js
+++ 2_JavaScript_Objects/function/Objects.js
@@ -25,6 +25,7 @@ function variavel() {
     console.log("Starting Video.");
   }
 
+
   let computer2 = {
     name: "Other computer",
     memorySpace: 2048,
@@ -44,3 +45,32 @@ function variavel() {
       console.log("Starting Video.");
     }
 }
+
+/*
+function Person(firstName, lastName) {
+  this.FirstName = firstName || "unknown";
+  this.LastName = lastName || "unknown";            
+}
+
+Person.prototype.getFullName = function () {
+  return this.FirstName + " " + this.LastName;
+}
+
+function Student(firstName, lastName, schoolName, grade)
+{
+    Person.call(this, firstName, lastName);
+
+    this.SchoolName = schoolName || "unknown";
+    this.Grade = grade || 0;
+}
+
+//Student.prototype = Person.prototype;
+Student.prototype = new Person();
+Student.prototype.constructor = Student;
+
+var std = new Student("James","Bond", "XYZ", 10);
+            
+alert(std.getFullName()); // James Bond
+alert(std instanceof Student); // true
+alert(std instanceof Person); // true
+*/
