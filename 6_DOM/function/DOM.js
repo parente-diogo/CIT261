52bba4c95b2c770a1ebf6854ac99e0a26428238f
Diogo Parente <31190447+parente-diogo@users.noreply.github.com>
2020-02-12 23:49:27 -0300

    a

--- 6_DOM/function/DOM.js
+++ 6_DOM/function/DOM.js
@@ -1,4 +1,4 @@
-// CREATE ELEMENT ------------------------------------ //
+// CREATE AND APPEND --------------------------------- //
 function buttonCreate() {
   var btn = document.createElement("BUTTON");
   btn.innerHTML = "New button";
@@ -10,4 +10,32 @@ function textCreate() {
   var parag = document.createElement("p");
   parag.innerHTML = text;
   document.getElementById("space1").appendChild(parag);
+}
+
+// INSERT -------------------------------------------- //
+function textInsert() {
+  var inputText = document.getElementById("inputBox2").value;
+  var inputPlace = document.getElementById("inputIndex").value;
+  var space = document.getElementById("space2");
+
+  var newPar = document.createElement("p");
+  var text = document.createTextNode(inputText);
+  newPar.appendChild(text);
+  space.insertBefore(newPar, space.childNodes[inputPlace]);
+}
+
+// REMOVE -------------------------------------------- //
+function textRemove() {
+  var removePlace = document.getElementById("removeIndex").value;
+  var space = document.getElementById("space2");
+  space.removeChild(space.childNodes[removePlace]);
+}
+
+// REPLACE ------------------------------------------- //
+function textReplace() {
+  var inputText3 = document.getElementById("inputBox3").value;
+  var inputReplace = document.getElementById("replaceIndex").value;
+
+  var repPar = document.getElementById("space2").childNodes[inputReplace];
+  repPar.replaceChild(inputText3, repPar.childNodes[inputReplace]);
 }
