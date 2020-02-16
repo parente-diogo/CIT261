d7c91f87445fd6093378ac74af9575890a53ad4d
Diogo Parente <31190447+parente-diogo@users.noreply.github.com>
2020-02-16 15:32:43 -0300

    a

---
+++ 7_CSS_Class/function/CSS_Class.js
@@ -0,0 +1,28 @@
+// -------------------------------------------------
+/*function idChange1() {
+  document.getElementById('texto2').style.color = "pink";
+}
+
+function idChange2() {
+  document.getElementById('texto2').style.color = "purple";  
+}
+
+function idChange3() {
+  document.getElementById('texto2').style.color = "black";  
+}*/
+
+// -------------------------------------------------
+function classChange1() {
+  var x = document.getElementsByClassName('texto1');
+  var i;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.backgroundColor = "blue";
+}
+
+function classChange1() {
+  
+}
+
+function classChange1() {
+  
+}
