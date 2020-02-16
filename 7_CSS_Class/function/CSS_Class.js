1dc2e7cf374e51b62735fd634545106fed65ca74
Diogo Parente <31190447+parente-diogo@users.noreply.github.com>
2020-02-16 15:45:40 -0300

    a

--- 7_CSS_Class/function/CSS_Class.js
+++ 7_CSS_Class/function/CSS_Class.js
@@ -1,28 +1,40 @@
 // -------------------------------------------------
-/*function idChange1() {
-  document.getElementById('texto2').style.color = "pink";
+function idChange1() {
+  document.getElementById('texto2').style.background = "pink";
 }
 
 function idChange2() {
-  document.getElementById('texto2').style.color = "purple";  
+  document.getElementById('texto2').style.background = "purple";
 }
 
 function idChange3() {
-  document.getElementById('texto2').style.color = "black";  
-}*/
+  document.getElementById('texto2').style.background = "black";
+}
 
 // -------------------------------------------------
 function classChange1() {
   var x = document.getElementsByClassName('texto1');
   var i;
   for (i = 0; i < x.length; i++) {
-    x[i].style.backgroundColor = "blue";
+    x[i].style.color = 'red';
+    x[i].style.background = "yellow";
+  }
 }
 
-function classChange1() {
-  
+function classChange2() {
+  var x = document.getElementsByClassName('texto1');
+  var i;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.color = 'green';
+    x[i].style.background = "orange";
+  }
 }
 
-function classChange1() {
-  
+function classChange3() {
+  var x = document.getElementsByClassName('texto1');
+  var i;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.color = 'blue';
+    x[i].style.background = "pink";
+  }
 }
