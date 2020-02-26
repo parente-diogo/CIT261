7f2e8c884dd56a9537dff34934e70368cbc29e30
Diogo Parente <31190447+parente-diogo@users.noreply.github.com>
2020-02-25 23:08:27 -0300

    a

--- 8_Transitions/function/trigger.js
+++ 8_Transitions/function/trigger.js
@@ -1,5 +1,41 @@
-function fadeOut() {
-  const square = document.getElementById("square1");
+/* SPEED BEHAVIOR - TRANSITIONS -----------------*/
+function changeColor() {
+  const square = document.getElementById("example5");
+  square.style.background = "orange";
+}
 
+function enlarge() {
+  const square = document.getElementById("example5");
+  square.style.width = "200px";
+  square.style.height = "200px";
+}
+
+function shrink() {
+  const square = document.getElementById("example5");
+  square.style.width = "50px";
+  square.style.height = "50px";
+}
+
+function fadeOut() {
+  const square = document.getElementById("example5");
   square.style.opacity = "0";
+}
+
+function resetBox1() {
+  const square = document.getElementById("example5");
+  square.style.background = "blue";
+  square.style.width = "100px";
+  square.style.height = "100px";
+  square.style.opacity = "1";
+}
+
+/* SPEED BEHAVIOR - TRANSITIONS -----------------*/
+function moveRight() {
+  const square = document.getElementById("example6");
+  square.style.animation = "move1 2s alternate infinite ease-in-out";
+}
+
+function moveRing1() {
+  const square = document.getElementById("example6");
+  square.style.animation = "move2 4s infinite linear";
 }
