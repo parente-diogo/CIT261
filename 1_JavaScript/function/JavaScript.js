4ce06d9892eb798d10c617290327a53faa78aae9
Diogo Parente <31190447+parente-diogo@users.noreply.github.com>
2020-03-22 21:53:50 -0300

    A

--- 1_JavaScript/function/JavaScript.js
+++ 1_JavaScript/function/JavaScript.js
@@ -46,3 +46,58 @@ function doWhileLoop() {
   document.getElementById("outDoWhileLoop").innerHTML = output;
 }
 
+// IF / ELSE IF / ELSE ----------------------------------
+function statements1() {  // IF-IF
+  var value1 = parseInt(document.getElementById('1val1').value);
+  var value2 = parseInt(document.getElementById('1val2').value);
+  var value3 = parseInt(document.getElementById('1val3').value);
+  var value4 = parseInt(document.getElementById('1val4').value);
+  var result1 = "FALSE";
+  var result2 = "FALSE";
+
+  if (value1 > value2) {
+    result1 = "TRUE";
+  }
+  if (value3 > value4) {
+    result2 = "TRUE";
+  }
+
+  document.getElementById("1result1").innerHTML = result1;
+  document.getElementById("1result2").innerHTML = result2;
+}
+
+function statements2() {  // IF-ELSE IF
+  var value1 = parseInt(document.getElementById('2val1').value);
+  var value2 = parseInt(document.getElementById('2val2').value);
+  var value3 = parseInt(document.getElementById('2val3').value);
+  var value4 = parseInt(document.getElementById('2val4').value);
+  var result1 = "FALSE";
+  var result2 = "FALSE";
+
+  if (value1 > value2) {
+    result1 = "TRUE";
+  }
+  else if (value3 > value4) {
+    result2 = "TRUE";
+  }
+
+  document.getElementById("2result1").innerHTML = result1;
+  document.getElementById("2result2").innerHTML = result2;
+}
+
+function statements3() {  // IF-ELSE
+  var value1 = parseInt(document.getElementById('3val1').value);
+  var value2 = parseInt(document.getElementById('3val2').value);
+  var result1 = "FALSE";
+  var result2 = "FALSE";
+
+  if (value1 > value2) {
+    result1 = "TRUE";
+  }
+  else {
+    result2 = "TRUE";
+  }
+
+  document.getElementById("3result1").innerHTML = result1;
+  document.getElementById("3result2").innerHTML = result2;
+}
