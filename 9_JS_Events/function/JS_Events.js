339513a98851f442ab51433f9e7d14a01ee188e5
Diogo Parente <31190447+parente-diogo@users.noreply.github.com>
2020-03-29 21:44:34 -0300

    a

---
+++ 9_JS_Events/function/JS_Events.js
@@ -0,0 +1,15 @@
+function funOnClick() {
+  alert("You just clicked the button.");
+}
+
+function funOnTouchStart() {
+  alert("You just clicked the button.");
+}
+
+function funOnEnd() {
+  alert("You just clicked the button.");
+}
+
+function funOnCancel() {
+  alert("You just clicked the button.");
+}
