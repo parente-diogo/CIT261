dec59f280246826c6e6b6c6ca7adc3fe49e4a4b0
Diogo Parente <31190447+parente-diogo@users.noreply.github.com>
2020-04-05 22:47:28 -0300

    a

--- 9_JS_Events/function/JS_Events.js
+++ 9_JS_Events/function/JS_Events.js
@@ -1,15 +1,43 @@
+let x1 = document.getElementById("animation1");
+let x2 = document.getElementById("animation2");
+let x3 = document.getElementById("animation3");
+
+x1.addEventListener("webkitAnimationStart", funStart);
+x2.addEventListener("webkitAnimationEnd", funEnd);
+x3.addEventListener("webkitAnimationIteration", funRepeat);
+
+function funStart() {
+  alert("The animation has just started.");
+}
+
+function funEnd() {
+  alert("The animation has just finished.");
+}
+
+function funRepeat() {
+  alert("The animation is repeating.");
+}
+
+
+
+
+
+
+
+
+
 function funOnClick() {
-  alert("You just clicked the button.");
+  alert("You clicked the button using the mouse.");
 }
 
 function funOnTouchStart() {
-  alert("You just clicked the button.");
+  alert("You just touched the button.");
 }
 
 function funOnEnd() {
-  alert("You just clicked the button.");
+  alert("You released the button.");
 }
 
 function funOnCancel() {
-  alert("You just clicked the button.");
+  alert("Something interrupted you pushing the button.");
 }
